6667bc1e62f489a345256cd1a3480e527882100f
KEXIN LI <danielleeprince@gmail.com>
2016-12-13 16:18:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+<head>
+    <title>Challenge 8</title>
+    <meta charset="UTF-8">
+    
+    <style>
+      
+        img {
+            height: 500px;
+            width: 800px;
+        }
+        
+        
+    </style>
+</head>
+
+<body>
+        
+   <?php
+        
+    $dir = 'images/';
+    $file_display = array('jpg', 'jpeg', 'png', 'gif');
+
+    if (file_exists($dir) == false)
+    {
+        echo 'Directory \''. $dir. '\' not found!';
+    } 
+    else 
+    {
+        $dir_contents = scandir($dir);
+
+        foreach ($dir_contents as $file) 
+        {
+            $file_type = strtolower(end(explode('.', $file)));
+
+            if ($file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true) 
+            {
+                echo '<img src="'. $dir. '/'. $file. '" alt="'. $file.'" />';
+            }
+        }
+    }
+        
+        
+        
+    ?>
+    
+  
+</body>
+
+</HTML>
+    
